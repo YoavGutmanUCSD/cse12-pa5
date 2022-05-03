729668a898ed06a2d38f14e7a5ae98c9ac933698
YoavGutmanUCSD <ygutman@ucsd.edu>
2022-05-03 09:32:12 -0700

    Revert "Blaming failure on array weirdness; replaced with variable"
    This reverts commit 7d71559be82997a6ea69783af0c4fb987677bbca.

--- src/SortingAlgorithmLectures.java
+++ src/SortingAlgorithmLectures.java
@@ -3,31 +3,30 @@ public class SortingAlgorithmLectures {
     // this method combines two arrays of integers, assuming they are sorted
     public static int[] combine(int[] lst1, int[] lst2){
         // store latest index of each lst
-        // int[] index = {0, 0};
-        int index0 = 0, index1 = 0;
+        int[] index = {0, 0};
         int[] combined = new int[lst1.length + lst2.length];
 
         // add all values in order of size to "combined" array
-        while(index0 < lst1.length && index1 < lst2.length){
+        while(index[0] < lst1.length && index[1] < lst2.length){
             // if lst1 has greater value, add lst1's value, otherwise add lst2's
-            if(lst1[index0] < lst1[index1]){
-                combined[index0 + index1] = lst1[index0];
-                index0++;
+            if(lst1[index[0]] < lst1[index[1]]){
+                combined[index[0] + index[1]] = lst1[index[0]];
+                index[0]++;
             }
             else{
-                combined[index0 + index1] = lst1[index1];
-                index1++;
+                combined[index[0] + index[1]] = lst1[index[1]];
+                index[1]++;
             }
         }
 
         // add leftovers
-        while(index0 < lst1.length){
-            combined[index0 + index1] = lst1[index0];
-            index0++;
+        while(index[0] < lst1.length){
+            combined[index[0] + index[1]] = lst1[index[0]];
+            index[0]++;
         }
-        while(index1 < lst2.length){
-            combined[index0 + index1] = lst1[index1];
-            index1++;
+        while(index[1] < lst2.length){
+            combined[index[0] + index[1]] = lst1[index[1]];
+            index[1]++;
         }
         return combined;
     }
