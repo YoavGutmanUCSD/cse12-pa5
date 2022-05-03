492529ab11fd312c635c2fcfb2f6def757958135
Yoav Gutman <ygutman@ucsd.edu>
2022-05-03 13:28:33 -0700

    First draft of FirstElePivotPartitioner with tests and new job in yml

--- src/FirstElePivotPartitioner.java
+++ src/FirstElePivotPartitioner.java
@@ -1,4 +1,37 @@
+import java.util.ArrayList;
 // You can (and should) add "implements Partitioner" below once you have the implementation ready
-public class FirstElePivotPartitioner {
+public class FirstElePivotPartitioner implements Partitioner {
+    @Override
+    public int partition(String[] strs, int low, int high){
+        // pivot point data
+        // int pivotIndex = low;
+        String pivotStr = strs[low];
 
+        // to hold everything smallerThan and greaterThan the pivotStr
+        ArrayList<String> smallerThan = new ArrayList<String>();
+        ArrayList<String> greaterThan = new ArrayList<String>();
+
+        // add to smallerThan if string length < pivotStr length
+        // otherwise add to greaterThan
+        // ignore if you encounter pivotStr 
+        for(String curr: strs){
+            if(curr.equals(pivotStr)) continue;
+            if(curr.length() < pivotStr.length()){
+                smallerThan.add(curr);
+            }
+            else {
+                greaterThan.add(curr);
+            }
+        }
+        int pivotIndex = smallerThan.size() + low;
+        String[] newStrs = new String[strs.length];
+        for(int i = low; i < smallerThan.size(); i++){
+            newStrs[i] = strs[i];
+        }
+        newStrs[pivotIndex] = pivotStr;
+        for(int j = smallerThan.size() + 1; j < greaterThan.size(); j++){
+            newStrs[j] = strs[j];
+        }
+        return pivotIndex;
+    }
 }
---
+++ src/TestPartitionerYoav.java
@@ -0,0 +1,33 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestPartitionerYoav {
+    FirstElePivotPartitioner e = new FirstElePivotPartitioner();
+
+    @Test
+    public void testSameLength(){
+        String[] strs = {"aaa", "a", "aa", "aaaa", "aaaaaa", "aaaa", "aaaaaaaa", "aaaaaaaaa"};
+        String[] strsPre = {"aaa", "a", "aa", "aaaa", "aaaaaa", "aaaa", "aaaaaaaa", "aaaaaaaaa"};
+        int index = e.partition(strs, 0, strs.length);
+        assertTrue(strs.length == strsPre.length);
+    }
+    @Test
+    public void testPrePivotAccuracy() {
+        String[] strs = {"aaa", "a", "aa", "aaaa", "aaaaaa", "aaaa", "aaaaaaaa", "aaaaaaaaa"};
+        String[] strsPre = {"aaa", "a", "aa", "aaaa", "aaaaaa", "aaaa", "aaaaaaaa", "aaaaaaaaa"};
+        int index = e.partition(strs, 0, strs.length);
+        for(int i = 0; i < index; i++){
+            assertTrue(strs[i].length <= strs[index]);
+        }
+    }
+    @Test
+    public void testPostPivotAccuracy() {
+        String[] strs = {"aaa", "a", "aa", "aaaa", "aaaaaa", "aaaa", "aaaaaaaa", "aaaaaaaaa"};
+        String[] strsPre = {"aaa", "a", "aa", "aaaa", "aaaaaa", "aaaa", "aaaaaaaa", "aaaaaaaaa"};
+        int index = e.partition(strs, 0, strs.length);
+        for(int i = index+1; i < strs.length; i++){
+            assertTrue(strs[i].length >= strs[index]);
+        }
+    }
+}
